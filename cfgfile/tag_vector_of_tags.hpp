f5befb2f5352ff59c608bdee28c65f934e39d792
Igor Mironchik <igor.mironchik@gmail.com>
2017-05-07 14:02:19 +0300

    Converted tag_vector_of_tags.hpp

--- cfgfile/tag_vector_of_tags.hpp
+++ cfgfile/tag_vector_of_tags.hpp
@@ -32,18 +32,19 @@
 #define CFGFILE__TAG_VECTOR_OF_TAGS_HPP__INCLUDED
 
 // cfgfile include.
-#include <cfgfile/private/Tag>
-#include <cfgfile/Exceptions>
+#include "tag.hpp"
+#include "exceptions.hpp"
+#include "types.hpp"
 
-// Qt include.
-#include <QtCore/QVector>
-#include <QtCore/QSharedPointer>
+// C++ include.
+#include <vector>
+#include <memory>
 
 
 namespace cfgfile {
 
 //
-// TagVectorOfTags
+// tag_vector_of_tags_t
 //
 
 /*!
@@ -52,35 +53,55 @@ namespace cfgfile {
 	Subordinate tag must have following constructor
 
 	\code
-		Tag( const QString & name, bool isMandatory );
+		tag_t( const string_t & name, bool is_mandatory );
 	\endcode
 */
 template< class T >
-class TagVectorOfTags
-	:	public Tag
+class tag_vector_of_tags_t
+	:	public tag_t
 {
 public:
 	//! Type of the pointer to the subordinate tag,
-	typedef QSharedPointer< T > PointerToTag;
+	typedef std::shared_ptr< T > ptr_to_tag_t;
 	//! Type of the vector of subordinate tags.
-	typedef QVector< PointerToTag > VectorOfTags;
+	typedef std::vector< ptr_to_tag_t > vector_of_tags_t;
 
-	explicit TagVectorOfTags( const QString & name, bool isMandatory = false );
-	TagVectorOfTags( Tag & owner, const QString & name, bool isMandatory = false );
+	explicit tag_vector_of_tags_t( const string_t & name,
+		bool is_mandatory = false )
+		:	tag_t( name, is_mandatory )
+	{
+	}
 
-	virtual ~TagVectorOfTags();
+	tag_vector_of_tags_t( tag_t & owner, const string_t & name,
+		bool is_mandatory = false )
+		:	tag_t( owner, name, is_mandatory )
+	{
+	}
+
+	~tag_vector_of_tags_t()
+	{
+	}
 
 	//! \return Amount of the subordinate tags.
-	int
-	size() const;
+	vector_of_tags_t::size_type
+	size() const
+	{
+		return m_tags.size();
+	}
 
 	//! \return Subordinate tag at the given position.
 	const T &
-	at( int index ) const;
+	at( vector_of_tags_t::size_type index ) const
+	{
+		return *m_tags.at( index );
+	}
 
 	//! \return Vector with subordinate tags.
-	const VectorOfTags &
-	values() const;
+	const vector_of_tags_t &
+	values() const
+	{
+		return m_tags;
+	}
 
 	/*!
 		Set value.
@@ -88,11 +109,21 @@ public:
 		Repeatly adds value to the end of vector.
 	*/
 	void
-	setValue( PointerToTag & p );
+	set_value( ptr_to_tag_t & p )
+	{
+		m_tags.push_back( p );
+
+		set_defined();
+	}
 
 	//! Set values.
 	void
-	setValues( const VectorOfTags & v );
+	set_values( const vector_of_tags_t & v )
+	{
+		m_tags = v;
+
+		set_defined();
+	}
 
 	/*!
 		Query optional values.
@@ -102,176 +133,90 @@ public:
 		otherwise nothing with \a receiver will happen.
 	*/
 	void
-	queryOptValues( VectorOfTags & receiver );
+	query_opt_values( vector_of_tags_t & receiver )
+	{
+		if( is_defined() )
+			receiver = m_tags;
+	}
 
 	//! \return List with children.
-	virtual const ChildTagsList & children() const;
+	const child_tags_list_t & children() const override
+	{
+		static const tag_t::child_tags_list_t empty;
+
+		if( m_current )
+			return m_current->children();
+		else
+			return empty;
+	}
 
 	//! Print tag to the output.
-	virtual QString print( int indent = 0 ) const;
+	string_t print( int indent = 0 ) const override
+	{
+		string_t result;
 
+		if( is_defined() )
+		{
+			for( const ptr_to_tag_t & p : m_tags )
+				result.push_back( p->print( indent ) );
+		}
+
+		return result;
+	}
+
+#if defined( CFGFILE_QSTRING_BUILD ) && defined( CFGFILE_XML_BUILD )
 	//! Print tag to the output.
-	virtual void print( QDomDocument & doc, QDomElement * parent = 0 ) const;
+	void print( QDomDocument & doc, QDomElement * parent = 0 ) const override
+	{
+		if( is_defined() )
+		{
+			for( const ptr_to_tag_t & p : m_tags )
+				p->print( doc, parent );
+		}
+	}
+#endif
 
 	//! Called when tag parsing started.
-	virtual void onStart( const ParserInfo & info );
+	void on_start( const parser_info_t & info ) override
+	{
+		m_current = std::make_shared< T > ( name(), is_mandatory() );
+		m_current->set_parent( parent() );
+		m_current->on_start( info );
+	}
 
 	//! Called when tag parsing finished.
-	virtual void onFinish( const ParserInfo & info );
+	void on_finish( const parser_info_t & info ) override
+	{
+		m_current->on_finish( info );
+		m_tags.push_back( m_current );
+		m_current.reset();
+
+		for( const tag_t * tag : children() )
+		{
+			if( tag->is_mandatory() && !tag->is_defined() )
+				throw exception_t( string_t( SL( "Undefined child mandatory tag: \"" ) ) +
+					tag->name() + SL( "\". Where parent is: \"" ) +
+					name() + SL( "\". In file \"" ) +
+					info.file_name() + SL( "\" on line " ) +
+					info.line_number() + SL( "." ) );
+		}
+
+		set_defined();
+	}
 
 	//! Called when string found.
-	virtual void onString( const ParserInfo & info,
-		const QString & str );
+	void on_string( const parser_info_t & info,
+		const string_t & str ) override
+	{
+		m_current->on_string( info, str );
+	}
 
 private:
 	//! Vector of subordinate tags.
-	VectorOfTags m_tags;
+	vector_of_tags_t m_tags;
 	//! Pointer to the current subordinate tag.
-	PointerToTag m_current;
-}; // class TagVectorOfTags
-
-
-template< class T >
-TagVectorOfTags< T >::TagVectorOfTags( const QString & name, bool isMandatory )
-	:	Tag( name, isMandatory )
-{
-}
-
-template< class T >
-TagVectorOfTags< T >::TagVectorOfTags( Tag & owner, const QString & name,
-	bool isMandatory )
-	:	Tag( owner, name, isMandatory )
-{
-}
-
-template< class T >
-TagVectorOfTags< T >::~TagVectorOfTags()
-{
-}
-
-template< class T >
-int
-TagVectorOfTags< T >::size() const
-{
-	return m_tags.size();
-}
-
-template< class T >
-const T &
-TagVectorOfTags< T >::at( int index ) const
-{
-	return *m_tags.at( index );
-}
-
-template< class T >
-const typename TagVectorOfTags< T >::VectorOfTags &
-TagVectorOfTags< T >::values() const
-{
-	return m_tags;
-}
-
-template< class T >
-void
-TagVectorOfTags< T >::setValue( PointerToTag & p )
-{
-	m_tags.push_back( p );
-
-	setDefined();
-}
-
-template< class T >
-void
-TagVectorOfTags< T >::setValues( const VectorOfTags & v )
-{
-	m_tags = v;
-
-	setDefined();
-}
-
-template< class T >
-void
-TagVectorOfTags< T >::queryOptValues( VectorOfTags & receiver )
-{
-	if( isDefined() )
-		receiver = m_tags;
-}
-
-template< class T >
-const Tag::ChildTagsList &
-TagVectorOfTags< T >::children() const
-{
-	static const Tag::ChildTagsList empty;
-
-	if( m_current )
-		return m_current->children();
-	else
-		return empty;
-}
-
-template< class T >
-QString
-TagVectorOfTags< T >::print( int indent ) const
-{
-	QString result;
-
-	if( isDefined() )
-	{
-		foreach( PointerToTag p, m_tags )
-			result.append( p->print( indent ) );
-	}
-
-	return result;
-}
-template< class T >
-void
-TagVectorOfTags< T >::print( QDomDocument & doc, QDomElement * parent ) const
-{
-	if( isDefined() )
-	{
-		foreach( PointerToTag p, m_tags )
-			p->print( doc, parent );
-	}
-}
-
-template< class T >
-void
-TagVectorOfTags< T >::onStart( const ParserInfo & info )
-{
-	m_current = PointerToTag( new T( name(), isMandatory() ) );
-	m_current->setParent( parent() );
-	m_current->onStart( info );
-}
-
-template< class T >
-void
-TagVectorOfTags< T >::onFinish( const ParserInfo & info )
-{
-	m_current->onFinish( info );
-	m_tags.push_back( m_current );
-	m_current.clear();
-
-	foreach( Tag * tag, children() )
-	{
-		if( tag->isMandatory() && !tag->isDefined() )
-			throw Exception( QString( "Undefined child mandatory tag: \"%1\". "
-				"Where parent is: \"%2\". "
-				"In file \"%3\" on line %4." )
-					.arg( tag->name() )
-					.arg( name() )
-					.arg( info.fileName() )
-					.arg( info.lineNumber() ) );
-	}
-
-	setDefined();
-}
-
-template< class T >
-void
-TagVectorOfTags< T >::onString( const ParserInfo & info,
-	const QString & str )
-{
-	m_current->onString( info, str );
-}
+	ptr_to_tag_t m_current;
+}; // class tag_vector_of_tags_t
 
 } /* namespace cfgfile */
 
